988142c6d43a102de5414990f64b13fec4b95825
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2014-06-05 09:49:30 +0400

    add AbstractSlider widget. Not implemented yet

--- vvv/engine/widgets/AbstractSlider.java
+++ vvv/engine/widgets/AbstractSlider.java
@@ -10,6 +10,77 @@ package vvv.engine.widgets;
  *
  * @author vvv
  */
-public class AbstractSlider {
+public class AbstractSlider 
+{
+    private int range;
+    private int value;
+    private int numTicks;
+
+    public AbstractSlider(int range, int numTicks)
+    {
+        setRange(range);
+        setNumTicks(numTicks);
+        setValue( range/2 );
+    }
+    
+    public AbstractSlider() 
+    {
+        this(100, 0);
+    }
+    
+    public AbstractSlider(int range)
+    {
+        this(range,0);
+    }
+    
+    public final void setRange(int range)
+    {
+        if( range <= 0 )
+        {
+            throw new IllegalArgumentException("Range should be greater than zero");
+        }
+        
+        if(     ( this.numTicks > 0 ) 
+             && ( range < numTicks-1) )
+        {
+            setNumTicks(range+1);
+        }
+        
+        this.range = range;
+    }
+    
+    public final void setNumTicks(int numTicks)
+    {
+        if( numTicks < 0 || numTicks > range + 1 )
+        {
+            throw new IllegalArgumentException( "numTicks should be ( 0 <= numTicks <= range+1)" );
+        }
+        this.numTicks = numTicks;
+    }
+    
+    public final void setValue( int value)
+    {
+                            // clamp value to [0..range]
+        this.value = Math.max(0, Math.min(range, value) );
+    }
+    
+    public final int getValue()
+    {
+        return this.value;
+    }
+    
+    public final int getRange()
+    {
+        return this.range;
+    }
+    
+    public final int getTicks()
+    {
+        return this.numTicks;
+    }
+    
+    
+    
+    
     
 }
