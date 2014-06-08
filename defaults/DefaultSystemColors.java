1997f52e24c52b69d72321024dd7691598e27300
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2014-06-08 11:53:43 +0400

    add new color control variables to DefaultSystemColors

--- defaults/DefaultSystemColors.java
+++ defaults/DefaultSystemColors.java
@@ -19,8 +19,13 @@ public class DefaultSystemColors
     private static final VariableColor controlColor = new VariableColor( ConstColor.WHITE );
     private static final VariableColor textColor   = new VariableColor( ConstColor.BLACK );
     
+    private static final VariableColor sliderActivePart  = new VariableColor( ConstColor.BLUE );
+    private static final VariableColor sliderPassivePart = new VariableColor( ConstColor.GRAY );
+    
     public static VariableColor getPanelColor() { return panelColor; }
     public static VariableColor getControlColor() { return controlColor; }
     public static VariableColor getTextColor() { return textColor; }
+    public static VariableColor getSliderActivePartColor() {return sliderActivePart; }
+    public static VariableColor getSliderPassivePartColor() {return sliderPassivePart; }
     
 }
